1800db2be71ff9215265c37056513060fffdd3a0
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-06-04 10:20:20 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,16 +1,16 @@
 class script(object):
     START = """**Hello {} 🤟**
    
-I am **Find Post Bot**.I am best Channel Link Search Bot! 
+I am **Request Movies Guardian Bot**.I am best Channel Link Search Bot! (Auto media filter bot)
 I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
 
     HELP = """To Use me In A Group
 
-- Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @unicornguardian.
-- After verification send /connect YourChannelID
+- Add me to your group & channel with all permissions. 
+- Send "/verify" in the group and wait for acceptance or contact the owner directly (@unicornguardian).
+- After verification, send /connect YourChannelID
 - Example : /connect -100xxxxxxxxxx
-- Done ✅. <b><I>Enjoy 💜❤</I></b> )
+- Done! Enjoy the enhanced features. ✅. <b><I>Enjoy 💜❤</I></b> )
 
 Remove a Channel with - /disconnect-100xxxxxxxxxxx
 this will help you to remove a indexed channel from your group.
@@ -20,7 +20,7 @@ Get connected channels list with - /connections"""
     ABOUT = """Contact To @unicornguardian
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='tg://user?id=1883570185'>Harman</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='tg://user?id=5503019033'>Unicorn</a>
 ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
 ✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
