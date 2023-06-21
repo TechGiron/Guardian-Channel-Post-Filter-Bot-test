cb5a07eec9a89c54ffb9afb62eec47948d0a671c
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-06-21 20:40:01 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -5,7 +5,8 @@ from time import time
 from client import User
 from pyrogram import Client, filters 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
-from thefuzz import fuzz, process
+from thefuzz import fuzz
+from thefuzz import process
 import re
 
 @Client.on_message(filters.text & filters.group & filters.incoming & ~filters.command(["verify", "connect", "id"]))
