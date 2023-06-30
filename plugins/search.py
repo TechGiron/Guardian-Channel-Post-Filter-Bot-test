f9d0333d2354718f1de99daa533ff05e290f23ed
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-06-30 17:11:54 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -23,8 +23,8 @@ async def search(bot, message):
     query_words = query.split()  # Split the query into individual words
     filtered_query_words = [word for word in query_words if word not in ["the", "dubbed", "movie", "download", "movies", "hindi", "english", "punjabi", "marathi", "tamil", "gujarati", "bengali", "kannada", "telugu", "malayalam", "to", "of", "org", "hd", "dub", "pls", "please",]and not re.match(r'^\d+$', word)]
     query = " ".join(filtered_query_words)  # Reconstruct the filtered query
-    start_time = time.time()  # Start measuring elapsed time
     results = ""
+    start_time = time.time()  # Start measuring elapsed time
     try:
        for channel in channels:
            async for msg in User.search_messages(chat_id=channel, query=query):
