687b2e886d93e0ffa6692cef60a53a043a8afd14
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-07-01 22:11:47 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,12 +7,12 @@ from pyrogram.errors import UserNotParticipant
 from motor.motor_asyncio import AsyncIOMotorClient
 from pyrogram.types import ChatPermissions, InlineKeyboardMarkup, InlineKeyboardButton
 
-dbclient    = AsyncIOMotorClient(DATABASE_URI)
-db          = dbclient["Channel-Filter"]
-grp_col     = db["GROUPS"]
-user_col    = db["USERS"]
-dlt_col     = db["Auto-Delete"]
-private_col = db["GROUPS"]
+dbclient = AsyncIOMotorClient(DATABASE_URI)
+db       = dbclient["Channel-Filter"]
+grp_col  = db["GROUPS"]
+user_col = db["USERS"]
+dlt_col  = db["Auto-Delete"]
+pm_col   = db["GROUPS"]
 
 ia = Cinemagoer()
 
@@ -147,7 +147,7 @@ async def broadcast_messages(user_id, message):
     except Exception as e:
         return False, "Error"
 
-async def add_private(user_id, user_name, channels, f_sub, verified):
+async def add_pm(user_id, user_name, channels, f_sub, verified):
     data = {
         "_id": user_id,
         "user_name": user_name,
@@ -156,38 +156,38 @@ async def add_private(user_id, user_name, channels, f_sub, verified):
         "verified": verified
     }
     try:
-        await private_col.insert_one(data)
+        await pm_col.insert_one(data)
     except DuplicateKeyError:
         pass
 
-async def get_private(id):
+async def get_pm(id):
     data = {'user_id': id}
-    private = await private_col.find_one(data)
-    return dict(private)
+    pm = await pm_col.find_one(data)
+    return dict(pm)
 
-async def update_private(id, new_data):
+async def update_pm(id, new_data):
     data = {"_id": id}
     new_value = {"$set": new_data}
-    await private_col.update_one(data, new_value)
+    await pm_col.update_one(data, new_value)
 
-async def delete_private(id):
+async def delete_pm(id):
     data = {"_id": id}
-    await private_col.delete_one(data)
+    await pm_col.delete_one(data)
 
 async def delete_user(id):
     data = {"user_id": id}
-    await private_col.delete_one(data)
+    await pm_col.delete_one(data)
 
-async def get_privates():
-    count = await private_col.count_documents({})
-    cursor = private_col.find({})
+async def get_pms():
+    count = await pm_col.count_documents({})
+    cursor = pm_col.find({})
     result = await cursor.to_list(length=int(count))
     return count, result
 
 async def force_sub(bot, message):
-    private = await get_private(message.from_user.id)
-    f_sub = private["f_sub"]
-    admin = private["user_id"]
+    pm = await get_pm(message.from_user.id)
+    f_sub = pm["f_sub"]
+    admin = pm["user_id"]
     if f_sub == False:
         return True
     if message.from_user is None:
@@ -196,7 +196,7 @@ async def force_sub(bot, message):
         f_link = (await bot.get_chat(f_sub)).invite_link
         member = await bot.get_chat_member(f_sub, message.from_user.id)
         if member.status == enums.ChatMemberStatus.BANNED:
-            await message.reply(f"ꜱᴏʀʀʏ {message.from_user.mention}!\n ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ, ʏᴏᴜ ᴡɪʟʟ ʙᴇ ʙᴀɴɴᴇᴅ ꜰʀᴏᴍ ʜᴇʀᴇ ᴡɪᴛʜɪɴ 10 ꜱᴇᴄᴏɴᴅꜱ")
+            await message.reply(f"ꜱᴏʀʀʏ {message.from_user.mention}!\n ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ, ʏᴏᴜ ᴡɪʟʟ ʙᴀɴɴᴇᴅ ꜰʀᴏᴍ ʜᴇʀᴇ ᴡɪᴛʜɪɴ 10 ꜱᴇᴄᴏɴᴅꜱ")
             await asyncio.sleep(10)
             await bot.ban_chat_member(message.chat.id, message.from_user.id)
             return False
