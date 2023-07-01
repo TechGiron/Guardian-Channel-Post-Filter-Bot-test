2887e6cbb7314583f90d03a22ef78647af9ed139
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-07-01 22:56:42 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -183,3 +183,42 @@ async def get_privates():
     cursor = private_col.find({})
     result = await cursor.to_list(length=int(count))
     return count, result
+
+async def force_sub(bot, message):
+    private = await get_private(message.from_user.id)
+    f_sub = private["f_sub"]
+    admin = private["user_id"]
+    if f_sub == False:
+        return True
+    if message.from_user is None:
+        return True
+    try:
+        f_link = (await bot.get_chat(f_sub)).invite_link
+        member = await bot.get_chat_member(f_sub, message.from_user.id)
+        if member.status == enums.ChatMemberStatus.BANNED:
+            await message.reply(f"ꜱᴏʀʀʏ {message.from_user.mention}!\n ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ, ʏᴏᴜ ᴡɪʟʟ ʙᴇ ʙᴀɴɴᴇᴅ ꜰʀᴏᴍ ʜᴇʀᴇ ᴡɪᴛʜɪɴ 10 ꜱᴇᴄᴏɴᴅꜱ")
+            await asyncio.sleep(10)
+            await bot.ban_chat_member(message.chat.id, message.from_user.id)
+            return False
+    except UserNotParticipant:
+        await bot.restrict_chat_member(
+            chat_id=message.chat.id,
+            user_id=message.from_user.id,
+            permissions=ChatPermissions(can_send_messages=False)
+        )
+        await message.reply(
+            f"<b>🚫 ʜɪ ᴅᴇᴀʀ {message.from_user.mention}!\n\n ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ꜱᴇɴᴅ ᴍᴇꜱꜱᴀɢᴇ ɪɴ ᴛʜɪꜱ ɢʀᴏᴜᴘ.. ᴛʜᴇɴ ꜰɪʀꜱᴛ ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴍᴇꜱꜱᴀɢᴇ ʜᴇʀᴇ 💯</b>",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("✅ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ✅", url=f_link)],
+                    [InlineKeyboardButton("🌀 ᴛʀʏ ᴀɢᴀɪɴ 🌀", callback_data=f"checksub_{message.from_user.id}")],
+                ]
+            ),
+        )
+        await message.delete()
+        return False
+    except Exception as e:
+        await bot.send_message(chat_id=admin, text=f"❌ Error in Fsub:\n`{str(e)}`")
+        return False
+    else:
+        return True
