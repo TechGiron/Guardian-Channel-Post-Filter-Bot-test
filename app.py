db2626634187edc3c4d57d81364fe67a4623379a
Rahul <111085537+TechGiron@users.noreply.github.com>
2024-08-01 21:07:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask
-import threading
-from client import Bot
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Bharat'
 
-def run_bot():
-    print("Bot Started 👍 Powered By @unicornguardian")
-    Bot().run()
 
 if __name__ == "__main__":
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
-    app.run(host='0.0.0.0', port=8000)
+    app.run()
