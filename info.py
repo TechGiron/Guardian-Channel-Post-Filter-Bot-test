e56b34280090d7051a0160e39901d1cdfc15fa4b
Rahul <111085537+TechGiron@users.noreply.github.com>
2024-08-14 14:18:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,5 +3,5 @@ API_HASH     = "5cbba7c97fe4bc32c5763f059d54917d"
 BOT_TOKEN    = "5813956283:AAEbzFBRDHkOwCDRWW_d3zWZOqGGzbLCYuY"
 SESSION      = "BQE-E7gAmVogQOTb-BB66MevwAmGOIAHyB7RzMZ5Jpy5UbT16GP1SjjCjnwe5Ab243cJ97FGWnDXRCWEMNWX328ujzlqqADj4XVKwU_hqFfrjx4lMLIgQjALK8c-m8e1x3w2sRjrJKmvL1tEwmwYg0tIcbLQ9ARhzVp4x8C80V7G4CTaczAbyJQxdFeWy1lsBzxKRgXrLel200CqAojcWSKCry0ol9InqIKap-hUB9F0xtmp0fjVobp68O2mz5gigD_bdsx7kaidXgt8WgnaDsmrKF2-41lWRxyml9iSQodIi0ycWX_-Ygm7jrF9rHIcWL4MAGqE5C-XdZOTnG_hf-pA764opQAAAABHTPEKAA"
 DATABASE_URI = "mongodb+srv://Guardian:03khd5TFckybkqKD@cluster0.lnqaahf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1002231340475
+LOG_CHANNEL  = -1001718845803
 ADMIN        = 5503019033
